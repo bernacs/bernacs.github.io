917f77e7fd2f109eb468a6b101a750c2925a0bfa
Bruno Klopott <bruno@riseup.net>
2015-11-07 22:26:29 +0100

    added google analytics tracking to assets/js/basic.js

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-		$('nav.sticky').fixTo('.main-container');
-	  $('nav').perfectScrollbar({
-			suppressScrollX: true
-	  });
+  $('nav.sticky').fixTo('.main-container');
+  $('nav').perfectScrollbar({
+    suppressScrollX: true
+  });
 
-	  $(window).resize(function(event) {
-	    $('nav').perfectScrollbar('update');
-	  });
+  $(window).resize(function(event) {
+    $('nav').perfectScrollbar('update');
+  });
 
-	  $(window).scroll(function(event) {
-	  	$('nav').perfectScrollbar('update')
-	  });
+  $(window).scroll(function(event) {
+    $('nav').perfectScrollbar('update')
+  });
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69836318-1', 'auto');
+  ga('send', 'pageview');
 });

f72c70e7cc5a98eea70673d828f4987ad91299c8
Bruno Klopott <bruno@riseup.net>
2015-11-08 20:32:05 +0100

    removing js sticky layout, trying out native position: sticky on other browsers (firefox works)

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-  $('nav').fixTo('.main-container');
+  // $('nav').fixTo('.main-container', {
+  //   'useNativeSticky': false
+  // });
+  // $('nav').pin();
+  // $('nav').sticky();
   // $('nav').stick_in_parent();
   $('nav').perfectScrollbar({
     suppressScrollX: true
